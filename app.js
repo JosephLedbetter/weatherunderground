6110ee9e8949b822a91ac14de924db43a7e92ad4
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-05-04 10:50:24 -0400

    changing location completed

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ document.addEventListener('DOMContentLoaded', getWeather)
 document.getElementById('w-change-btn').addEventListener('click', e => {
     const city = document.getElementById('city').value
     const country = document.getElementById('country').value
+
+    //change location
+    weather.changeLocation(city, country)
+
+    //set location in local storage
+    storage.setLocationData(city, country)
+
+    //get and display weather
+    getWeather()
+
+    //close modal
+    $('#locModal').modal('hide')
+})

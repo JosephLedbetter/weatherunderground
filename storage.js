e8031f184150d3783a5e51996731407ac8b2d129
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-05-04 10:51:52 -0400

    complete get location data completed

--- storage.js
+++ storage.js
@@ -5,4 +5,12 @@ class Storage {
       this.defaultCity = 'London'
       this.defaultState = 'UK'
     }
-  
+    getLocationData() {
+        localStorage.getItem('city') === null
+          ? (this.city = this.defaultCity)
+          : (this.city = localStorage.getItem('city'))
+    
+        localStorage.getItem('country') === null
+          ? (this.country = this.defaultState)
+          : (this.country = localStorage.getItem('country'))
+    

03f3a6785ff6b25cb20825719727ec24aaa0811f
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-05-04 11:08:50 -0400

    fetch completed for the weather from the api url

--- weather.js
+++ weather.js
@@ -5,3 +5,14 @@ class Weather {
         this.city = city
         this.country = country
     }
+  //fetch weather from api
+  async getWeather() {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.apiKey}&units=metric`
+    )
+
+    const responseData = await response.json()
+    console.log(responseData)
+
+    return responseData
+}
